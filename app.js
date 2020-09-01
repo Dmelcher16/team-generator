49a008923f9974675ddb2bcc43005c2475cb1ef4
Dak <67485000+Dmelcher16@users.noreply.github.com>
2020-08-31 18:19:31 -0700

    changed prompts to functions and made then loop

--- app.js
+++ app.js
@@ -4,11 +4,39 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+const {
+    addEmployeePrompt,
+    internPrompt,
+    engineerPrompt,
+    managerPrompt,
+} = require("./lib/prompt")
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+
+managerPrompt().then(function(manager){
+    console.log(manager);
+    main();
+});
+
+async function main() {
+    const {teamMember} = await addEmployeePrompt();
+    if (teamMember === "Engineer"){
+        const engineer = await engineerPrompt();
+        console.log(engineer);
+        main();
+    } else if (teamMember === "Intern"){
+        const intern = await internPrompt();
+        console.log(intern);
+        main();
+    } else {
+        // write to file
+    }
+}
+
+
 
 
 // Write code to use inquirer to gather information about the development team members,
@@ -34,117 +62,4 @@ const render = require("./lib/htmlRenderer");
 // object with the correct structure and methods. This structure will be crucial in order
 // for the provided `render` function to work! ```
 
-function prompts() {
-    return (inquirer.prompt( [
-        {
-            name: "ManagerName",
-            type: "input",
-            message: "What is your manager's name?"
-        }, {
-            name: "ManagerID",
-            type: "input",
-            message: "What is your manager's ID?"
-        }, {
-            name: "ManagerEmail",
-            type: "input",
-            message: "What is your manager's Email?"
-        }, {
-            name: "ManagerOfficeNumber",
-            type: "input",
-            message: "What is your manager's office number?"
-        },
-        {
-            type: "list",
-            message: "Which type of team member would you like to add?",
-            name: "teamMember",
-            choices: ["Engineer", "Intern"]
-        }, {
-            when: function(answers) {
-                return answers.teamMember === "Engineer";
-            },
-            name: "EngineerName",
-            type: "input",
-            message: "What is your engineer's name?",
-        }, {
-            when: function(answers) {
-                return answers.teamMember === "Engineer";
-            },
-            name: "EngineerID",
-            type: "input",
-            message: "What is your engineer's ID?",
-        }, {
-            when: function(answers) {
-                return answers.teamMember === "Engineer";
-            },
-            name: "EngineerEmail",
-            type: "input",
-            message: "What is your engineer's Email?",
-        }, {
-            when: function(answers) {
-                return answers.teamMember === "Engineer";
-            },
-            name: "EngineerGithub",
-            type: "input",
-            message: "What is your engineer's GitHub username?"
-        }, {
-            when: function(answers) {
-                return answers.teamMember === "Intern";
-            },
-            name: "InternName",
-            type: "input",
-            message: "What is your intern's name?",
-        }, {
-            when: function(answers) {
-                return answers.teamMember === "Intern";
-            },
-            name: "InternID",
-            type: "input",
-            message: "What is your intern's ID?",
-        }, {
-            when: function(answers) {
-                return answers.teamMember === "Intern";
-            },
-            name: "InternEmail",
-            type: "input",
-            message: "What is your intern's Email?",
-        }, {
-            when: function(answers) {
-                return answers.teamMember === "Intern";
-            },
-            name: "InternSchool",
-            type: "input",
-            message: "What school does your intern attend?",
-        }, 
-        
-        {
-            name: "add",
-            type: "confirm",
-            message: "Would you like to add another team member?",
-            then: function(confirmed) {
-                if (true){
-                    return prompts()
-                } else{
-                    return "Your document is being created"
-                }
-            }
-        }
 
-
-        // {
-        //     type: "input",
-        //     message: "What is your GitHub username?",
-        //     name: "username",
-        // },
-        // {
-        //     type: "list",
-        //     message: "What kind of license should your project have?",
-        //     name: "license",
-        //     choices: ["MIT", "GPL", "Apache", "unlicensed"]
-        // },
-       
-        
-        ])
-    )
-};
-
-prompts();
