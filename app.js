a17a5c0ca13ce29406493c5b6eff91984418dc62
Dak <67485000+Dmelcher16@users.noreply.github.com>
2020-09-01 15:07:43 -0700

    fixed function for prompts. now renders to page

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+const Employee = require("./lib/Employee")
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
@@ -15,9 +16,12 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+const team = [];
 
 managerPrompt().then(function(manager){
     console.log(manager);
+    team.push(manager)
+    
     main();
 });
 
@@ -26,15 +30,27 @@ async function main() {
     if (teamMember === "Engineer"){
         const engineer = await engineerPrompt();
         console.log(engineer);
+        team.push(engineer)
+        
         main();
     } else if (teamMember === "Intern"){
         const intern = await internPrompt();
         console.log(intern);
+        team.push(intern)
+        
         main();
     } else {
         // write to file
+        fs.writeFile(outputPath, render(team), function (err) {
+
+            if (err) {
+                return console.log(err);
+            }
+    
+        });
     }
 }
+
 
 
 
